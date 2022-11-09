e4d62269cc3ce2101e51cfc428ba402fe98900f1
Austin See <halfblowncontact@gmail.com>
2022-11-09 12:17:34 -0600

    add tests for md5-sess algo type

--- index.test.ts
+++ index.test.ts
@@ -1,119 +1,238 @@
 import axios, { AxiosError } from 'axios';
 import { AxiosDigest as DigestAuth } from './index';
 
+const URL_md5 = 'http://localhost:1337/md5';
+const URL_md5Sess = 'http://localhost:1337/md5-sess';
+
 describe('Axios Digest Tests', () => {
     describe('MD5', () => {
         describe('GET', () => {
             it('should succeed with correct username and password', async () => {
-                const axiosDigest = new DigestAuth('test', 'test', axios.create(), { retry: true });
-                const res = await axiosDigest.get('http://localhost:1337/testing');
+                const axiosDigest = new DigestAuth('test', 'test', axios.create());
+                const res = await axiosDigest.get(URL_md5);
                 expect(res).toBeDefined();
             });
             it('should fail with incorrect password', async () => {
                 expect(async () => {
-                    const axiosDigest = new DigestAuth('test', 'wrong-password', axios.create(), { retry: true });
-                    await axiosDigest.get('http://localhost:1337/testing');
+                    const axiosDigest = new DigestAuth('test', 'wrong-password', axios.create());
+                    await axiosDigest.get(URL_md5);
                 }).rejects.toThrow(AxiosError);
             });
             it('should fail with incorrect username', async () => {
                 expect(async () => {
-                    const axiosDigest = new DigestAuth('wrong-username', 'test', axios.create(), { retry: true });
-                    await axiosDigest.get('http://localhost:1337/testing');
+                    const axiosDigest = new DigestAuth('wrong-username', 'test', axios.create());
+                    await axiosDigest.get(URL_md5);
                 }).rejects.toThrow(AxiosError);
             });
         });
         describe('POST', () => {
             it('should succeed with correct username and password', async () => {
-                const axiosDigest = new DigestAuth('test', 'test', axios.create(), { retry: true });
-                const res = await axiosDigest.post('http://localhost:1337/testing', { test: true });
+                const axiosDigest = new DigestAuth('test', 'test', axios.create());
+                const res = await axiosDigest.post(URL_md5, { test: true });
                 expect(res).toBeDefined();
             });
             it('should fail with incorrect password', async () => {
                 expect(async () => {
-                    const axiosDigest = new DigestAuth('test', 'wrong-password', axios.create(), { retry: true });
-                    await axiosDigest.post('http://localhost:1337/testing', { test: true });
+                    const axiosDigest = new DigestAuth('test', 'wrong-password', axios.create());
+                    await axiosDigest.post(URL_md5, { test: true });
                 }).rejects.toThrow(AxiosError);
             });
             it('should fail with incorrect username', async () => {
                 expect(async () => {
-                    const axiosDigest = new DigestAuth('wrong-username', 'test', axios.create(), { retry: true });
-                    await axiosDigest.post('http://localhost:1337/testing', { test: true });
+                    const axiosDigest = new DigestAuth('wrong-username', 'test', axios.create());
+                    await axiosDigest.post(URL_md5, { test: true });
                 }).rejects.toThrow(AxiosError);
             });
         });
         describe('PATCH', () => {
             it('should succeed with correct username and password', async () => {
-                const axiosDigest = new DigestAuth('test', 'test', axios.create(), { retry: true });
-                const res = await axiosDigest.patch('http://localhost:1337/testing', { test: true });
+                const axiosDigest = new DigestAuth('test', 'test', axios.create());
+                const res = await axiosDigest.patch(URL_md5, { test: true });
                 expect(res).toBeDefined();
             });
             it('should fail with incorrect password', async () => {
                 expect(async () => {
-                    const axiosDigest = new DigestAuth('test', 'wrong-password', axios.create(), { retry: true });
-                    await axiosDigest.patch('http://localhost:1337/testing', { test: true });
+                    const axiosDigest = new DigestAuth('test', 'wrong-password', axios.create());
+                    await axiosDigest.patch(URL_md5, { test: true });
                 }).rejects.toThrow(AxiosError);
             });
             it('should fail with incorrect username', async () => {
                 expect(async () => {
-                    const axiosDigest = new DigestAuth('wrong-username', 'test', axios.create(), { retry: true });
-                    await axiosDigest.patch('http://localhost:1337/testing', { test: true });
+                    const axiosDigest = new DigestAuth('wrong-username', 'test', axios.create());
+                    await axiosDigest.patch(URL_md5, { test: true });
                 }).rejects.toThrow(AxiosError);
             });
         });
         describe('PUT', () => {
             it('should succeed with correct username and password', async () => {
-                const axiosDigest = new DigestAuth('test', 'test', axios.create(), { retry: true });
-                const res = await axiosDigest.put('http://localhost:1337/testing', { test: true });
+                const axiosDigest = new DigestAuth('test', 'test', axios.create());
+                const res = await axiosDigest.put(URL_md5, { test: true });
                 expect(res).toBeDefined();
             });
             it('should fail with incorrect password', async () => {
                 expect(async () => {
-                    const axiosDigest = new DigestAuth('test', 'wrong-password', axios.create(), { retry: true });
-                    await axiosDigest.put('http://localhost:1337/testing', { test: true });
+                    const axiosDigest = new DigestAuth('test', 'wrong-password', axios.create());
+                    await axiosDigest.put(URL_md5, { test: true });
                 }).rejects.toThrow(AxiosError);
             });
             it('should fail with incorrect username', async () => {
                 expect(async () => {
-                    const axiosDigest = new DigestAuth('wrong-username', 'test', axios.create(), { retry: true });
-                    await axiosDigest.put('http://localhost:1337/testing', { test: true });
+                    const axiosDigest = new DigestAuth('wrong-username', 'test', axios.create());
+                    await axiosDigest.put(URL_md5, { test: true });
                 }).rejects.toThrow(AxiosError);
             });
         });
         describe('DELETE', () => {
             it('should succeed with correct username and password', async () => {
-                const axiosDigest = new DigestAuth('test', 'test', axios.create(), { retry: true });
-                const res = await axiosDigest.delete('http://localhost:1337/testing');
+                const axiosDigest = new DigestAuth('test', 'test', axios.create());
+                const res = await axiosDigest.delete(URL_md5);
                 expect(res).toBeDefined();
             });
             it('should fail with incorrect password', async () => {
                 expect(async () => {
-                    const axiosDigest = new DigestAuth('test', 'wrong-password', axios.create(), { retry: true });
-                    await axiosDigest.delete('http://localhost:1337/testing');
+                    const axiosDigest = new DigestAuth('test', 'wrong-password', axios.create());
+                    await axiosDigest.delete(URL_md5);
                 }).rejects.toThrow(AxiosError);
             });
             it('should fail with incorrect username', async () => {
                 expect(async () => {
-                    const axiosDigest = new DigestAuth('wrong-username', 'test', axios.create(), { retry: true });
-                    await axiosDigest.delete('http://localhost:1337/testing');
+                    const axiosDigest = new DigestAuth('wrong-username', 'test', axios.create());
+                    await axiosDigest.delete(URL_md5);
                 }).rejects.toThrow(AxiosError);
             });
         });
         describe('HEAD', () => {
             it('should succeed with correct username and password', async () => {
-                const axiosDigest = new DigestAuth('test', 'test', axios.create(), { retry: true });
-                const res = await axiosDigest.head('http://localhost:1337/testing');
+                const axiosDigest = new DigestAuth('test', 'test', axios.create());
+                const res = await axiosDigest.head(URL_md5);
                 expect(res).toBeDefined();
             });
             it('should fail with incorrect password', async () => {
                 expect(async () => {
-                    const axiosDigest = new DigestAuth('test', 'wrong-password', axios.create(), { retry: true });
-                    await axiosDigest.head('http://localhost:1337/testing');
+                    const axiosDigest = new DigestAuth('test', 'wrong-password', axios.create());
+                    await axiosDigest.head(URL_md5);
                 }).rejects.toThrow(AxiosError);
             });
             it('should fail with incorrect username', async () => {
                 expect(async () => {
-                    const axiosDigest = new DigestAuth('wrong-username', 'test', axios.create(), { retry: true });
-                    await axiosDigest.head('http://localhost:1337/testing');
+                    const axiosDigest = new DigestAuth('wrong-username', 'test', axios.create());
+                    await axiosDigest.head(URL_md5);
+                }).rejects.toThrow(AxiosError);
+            });
+        });
+    });
+    describe('MD5-SESS', () => {
+        describe('GET', () => {
+            it('should succeed with correct username and password', async () => {
+                const axiosDigest = new DigestAuth('test', 'test', axios.create());
+                const res = await axiosDigest.get(URL_md5Sess);
+                expect(res).toBeDefined();
+            });
+            it('should fail with incorrect password', async () => {
+                expect(async () => {
+                    const axiosDigest = new DigestAuth('test', 'wrong-password', axios.create());
+                    await axiosDigest.get(URL_md5Sess);
+                }).rejects.toThrow(AxiosError);
+            });
+            it('should fail with incorrect username', async () => {
+                expect(async () => {
+                    const axiosDigest = new DigestAuth('wrong-username', 'test', axios.create());
+                    await axiosDigest.get(URL_md5Sess);
+                }).rejects.toThrow(AxiosError);
+            });
+        });
+        describe('POST', () => {
+            it('should succeed with correct username and password', async () => {
+                const axiosDigest = new DigestAuth('test', 'test', axios.create());
+                const res = await axiosDigest.post(URL_md5Sess, { test: true });
+                expect(res).toBeDefined();
+            });
+            it('should fail with incorrect password', async () => {
+                expect(async () => {
+                    const axiosDigest = new DigestAuth('test', 'wrong-password', axios.create());
+                    await axiosDigest.post(URL_md5Sess, { test: true });
+                }).rejects.toThrow(AxiosError);
+            });
+            it('should fail with incorrect username', async () => {
+                expect(async () => {
+                    const axiosDigest = new DigestAuth('wrong-username', 'test', axios.create());
+                    await axiosDigest.post(URL_md5Sess, { test: true });
+                }).rejects.toThrow(AxiosError);
+            });
+        });
+        describe('PATCH', () => {
+            it('should succeed with correct username and password', async () => {
+                const axiosDigest = new DigestAuth('test', 'test', axios.create());
+                const res = await axiosDigest.patch(URL_md5Sess, { test: true });
+                expect(res).toBeDefined();
+            });
+            it('should fail with incorrect password', async () => {
+                expect(async () => {
+                    const axiosDigest = new DigestAuth('test', 'wrong-password', axios.create());
+                    await axiosDigest.patch(URL_md5Sess, { test: true });
+                }).rejects.toThrow(AxiosError);
+            });
+            it('should fail with incorrect username', async () => {
+                expect(async () => {
+                    const axiosDigest = new DigestAuth('wrong-username', 'test', axios.create());
+                    await axiosDigest.patch(URL_md5Sess, { test: true });
+                }).rejects.toThrow(AxiosError);
+            });
+        });
+        describe('PUT', () => {
+            it('should succeed with correct username and password', async () => {
+                const axiosDigest = new DigestAuth('test', 'test', axios.create());
+                const res = await axiosDigest.put(URL_md5Sess, { test: true });
+                expect(res).toBeDefined();
+            });
+            it('should fail with incorrect password', async () => {
+                expect(async () => {
+                    const axiosDigest = new DigestAuth('test', 'wrong-password', axios.create());
+                    await axiosDigest.put(URL_md5Sess, { test: true });
+                }).rejects.toThrow(AxiosError);
+            });
+            it('should fail with incorrect username', async () => {
+                expect(async () => {
+                    const axiosDigest = new DigestAuth('wrong-username', 'test', axios.create());
+                    await axiosDigest.put(URL_md5Sess, { test: true });
+                }).rejects.toThrow(AxiosError);
+            });
+        });
+        describe('DELETE', () => {
+            it('should succeed with correct username and password', async () => {
+                const axiosDigest = new DigestAuth('test', 'test', axios.create());
+                const res = await axiosDigest.delete(URL_md5Sess);
+                expect(res).toBeDefined();
+            });
+            it('should fail with incorrect password', async () => {
+                expect(async () => {
+                    const axiosDigest = new DigestAuth('test', 'wrong-password', axios.create());
+                    await axiosDigest.delete(URL_md5Sess);
+                }).rejects.toThrow(AxiosError);
+            });
+            it('should fail with incorrect username', async () => {
+                expect(async () => {
+                    const axiosDigest = new DigestAuth('wrong-username', 'test', axios.create());
+                    await axiosDigest.delete(URL_md5Sess);
+                }).rejects.toThrow(AxiosError);
+            });
+        });
+        describe('HEAD', () => {
+            it('should succeed with correct username and password', async () => {
+                const axiosDigest = new DigestAuth('test', 'test', axios.create());
+                const res = await axiosDigest.head(URL_md5Sess);
+                expect(res).toBeDefined();
+            });
+            it('should fail with incorrect password', async () => {
+                expect(async () => {
+                    const axiosDigest = new DigestAuth('test', 'wrong-password', axios.create());
+                    await axiosDigest.head(URL_md5Sess);
+                }).rejects.toThrow(AxiosError);
+            });
+            it('should fail with incorrect username', async () => {
+                expect(async () => {
+                    const axiosDigest = new DigestAuth('wrong-username', 'test', axios.create());
+                    await axiosDigest.head(URL_md5Sess);
                 }).rejects.toThrow(AxiosError);
             });
         });
--- test-server/index.js
+++ test-server/index.js
@@ -4,18 +4,36 @@ const auth = require('http-auth');
 const PORT = 1337;
 
 const init = () => {
-    const digest = auth.digest({
+    const md5 = auth.digest({
         realm: 'users',
         file: __dirname + '/users.htdigest',
+        algorithm: 'MD5',
+    });
+    const md5Sess = auth.digest({
+        realm: 'users',
+        file: __dirname + '/users.htdigest',
+        algorithm: 'MD5-sess',
     });
 
-    http.createServer(
-        digest.check((req, res) => {
-            console.log('Digest-Auth header: ', req.headers.authorization);
-            console.log('Response status code: ', res.statusCode);
-            return res.end(`Welcome to private area - ${req.user}!`);
-        }),
-    ).listen(PORT, () => {
+    const cb = (req, res) => {
+        console.log('Digest-Auth header: ', req.headers.authorization);
+        console.log('Response status code: ', res.statusCode);
+        return res.end(`Welcome to private area - ${req.user}!`);
+    };
+
+    const requestListener = (req, res) => {
+        let func;
+        switch (req.url) {
+            case '/md5-sess':
+                func = md5Sess.check(cb);
+            case '/md5':
+            default:
+                func = md5.check(cb);
+        }
+        func(req, res);
+    };
+
+    http.createServer(requestListener).listen(PORT, () => {
         console.log(`listening on port: ${PORT}`);
     });
 };

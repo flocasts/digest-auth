f19f43063a9f88e57cedd9f5fc3a256088bff4a2
Austin See <halfblowncontact@gmail.com>
2023-11-17 13:19:02 -0800

    add deploy workflow, use types instead of enums

--- src/base/base.interface.ts
+++ src/base/base.interface.ts
@@ -3,9 +3,9 @@ import { AxiosInstance, AxiosStatic } from 'axios';
 /**
  * Supported HTTP clients
  */
-export interface HTTPClient {
+export type HTTPClient = {
     axios: AxiosInstance | AxiosStatic;
-}
+};
 /**
  * Utility type for type-safe inheritance of the base digest class
  */
@@ -14,27 +14,37 @@ export type SupportedHTTPClient = HTTPClient['axios'];
 /**
  * Supported HTTP methods
  */
-export enum Method {
-    GET = 'GET',
-    POST = 'POST',
-    PATCH = 'PATCH',
-    PUT = 'PUT',
-    DELETE = 'DELETE',
-    HEAD = 'HEAD',
-}
+export const Method = {
+    GET: 'GET',
+    POST: 'POST',
+    PATCH: 'PATCH',
+    PUT: 'PUT',
+    DELETE: 'DELETE',
+    HEAD: 'HEAD',
+} as const;
+
+/**
+ * Supported HTTP methods
+ */
+export type Method = typeof Method[keyof typeof Method];
 
 /**
  * Supported Algorithms used for digest authentication
  */
-export enum Algorithm {
-    MD5 = 'md5',
-}
+export const Algorithm = {
+    MD5: 'md5',
+} as const;
+
+/**
+ * Supported Algorithms used for digest authentication
+ */
+export type Algorithm = typeof Algorithm[keyof typeof Algorithm];
 
 /**
  * Shape of `www-authenticate` header object
  * https://httpwg.org/specs/rfc9110.html#field.www-authenticate
  */
-export interface AuthDetails {
+export type AuthDetails = {
     realm: string;
     nonce: string;
     qop?: string;
@@ -44,4 +54,4 @@ export interface AuthDetails {
     domain?: string; // not implemented, barely adopted by anything
     charset?: string; // not implemented, barely adopted by anything
     userhash?: string; // not implemented, barely adopted by anything
-}
+};
